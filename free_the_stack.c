2a9c05976f7a4a8382472ef9e7d4a808e4041449
chloe0524 <chloco31@gmail.com>
2024-01-02 07:16:55 -0800

    created free_stack function

---
+++ free_the_stack.c
@@ -0,0 +1,21 @@
+#include "monthy.h"
+
+/**
+ * free_the_stack - free memory allocated for stack
+ *
+ * @stack: pointer to top of the stack
+ *
+ */
+
+void free_the_stack(stack_t *stack)
+{
+	stack_t *current_node = NULL;
+
+	while (stack != NULL)
+	{
+		current_node = stack;
+
+		stack = stack->next;
+		free(current_node);
+	}
+}
