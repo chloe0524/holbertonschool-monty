4d736343299b4c328734a0cf27961239fbf75698
chloe0524 <chloco31@gmail.com>
2024-01-03 09:51:25 -0800

    missed error message

--- pint_op.c
+++ pint_op.c
@@ -10,5 +10,10 @@ void pint_op(stack_t **stack, unsigned int line_number)
 {
 	(void) line_number;
 
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	printf("%d\n", (*stack)->n);
 }
