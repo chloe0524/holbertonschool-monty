d6127847f69051264caf50f8dc23815ea3cf0469
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-01-03 19:12:31 +0100

    Update pop_op.c

--- pop_op.c
+++ pop_op.c
@@ -6,19 +6,16 @@
  * @stack: a pointer to the top of the stack.
  * @line_number: current line number
  */
-void pop(stack_t **stack, unsigned int line_number)
+void pop_op(stack_t **stack, unsigned int line_number)
 {
-        stack_t *now_node = NULL;
-
-        now_node = *stack;
-
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-        now_node = now_node->next;
-        free(*stack);
-        *stack = now_node;
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack_t *now_node = *stack;
+    *stack = (*stack)->next;
+    free(now_node);
 }
+
 
