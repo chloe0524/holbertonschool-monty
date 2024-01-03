87695f2a8d93b02dab340da99f08b11375d8226c
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-01-03 19:11:06 +0100

    Update pop_op.c merge issue

--- pop_op.c
+++ pop_op.c
@@ -6,20 +6,19 @@
  * @stack: a pointer to the top of the stack.
  * @line_number: current line number
  */
-void pop_op(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+        stack_t *now_node = NULL;
 
-	stack_t *bye_node = *stack;
-	*stack = (*stack)->next;
+        now_node = *stack;
 
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-
-	free(bye_node);
+        if (*stack == NULL)
+        {
+                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+        now_node = now_node->next;
+        free(*stack);
+        *stack = now_node;
 }
 
