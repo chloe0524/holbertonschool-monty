cf5bb4902440aaa2326e64540528aba79713f335
chloe0524 <chloco31@gmail.com>
2024-01-04 04:29:15 -0800

    added add_op

--- monty.h
+++ monty.h
@@ -49,6 +49,7 @@ void pint_op(stack_t **stack, unsigned int line_number);
 void pop_op(stack_t **stack, unsigned int line_number);
 void nop_op(stack_t **stack, unsigned int line_number);
 void swap_op(stack_t **stack, unsigned int line_number);
+void add_op(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_HEADER */
 
