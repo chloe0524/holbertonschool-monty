707bdf433e3eb528027cb6e13b9af8c26efedf6a
chloe0524 <chloco31@gmail.com>
2024-01-04 02:30:50 -0800

    fixed issue with frees and removed debug printf

--- pop_op.c
+++ pop_op.c
@@ -6,16 +6,21 @@
  * @stack: a pointer to the top of the stack.
  * @line_number: current line number
  */
+
 void pop_op(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	stack_t *now_node = *stack;
+
+	if ((*stack)->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+		return;
+	}
 	*stack = (*stack)->next;
-	free(now_node);
+	(*stack)->prev = NULL;
 }
-
-
