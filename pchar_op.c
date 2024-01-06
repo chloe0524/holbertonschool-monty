807a64d63455a6c9fd844589aeb5253066356a01
chloe0524 <chloco31@gmail.com>
2024-01-06 06:45:19 -0800

    update stack ptr at end

--- pchar_op.c
+++ pchar_op.c
@@ -8,23 +8,22 @@
  */
 void pchar_op(stack_t **stack, unsigned int line_number)
 {
+	char ascii_char;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%i: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
+	ascii_char = (*stack)->n;
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%i: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	putchar((*stack)->n);
+	putchar(ascii_char);
 	putchar('\n');
-
-	*stack = (*stack)->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
+	*stack = (*stack)->n->next;
 }
 
